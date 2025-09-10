8e2787b1b7e3121bd76746da00cf9787cff64772
Gian <gian.villca@sistemas.edu.bo>
2025-09-10 12:15:10 -0700

    Archivo con base de datos mod

--- database.py
+++ database.py
@@ -11,3 +11,17 @@ nombre=CharField(unique=True)
 
 class Meta:
 database=db
+
+#Tabla Participante
+class Participante(Model):
+carnet = CharField(unique=True)
+nombre=CharField()
+dia_evento=DateField()
+tipo_evento=ForeignKeyField(TipoEvento,backref='participantes')
+
+class Meta:
+database = db
+
+#conectar y crear las tablas
+db.connect()
+db.create_tables(T
